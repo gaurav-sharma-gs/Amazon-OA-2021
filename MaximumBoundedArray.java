076887578d94a313a1bdf65cf83e8539580a57c2
Gaurav Sharma <gaurav.gs@flipkart.com>
2021-02-10 21:42:24 -0500

    Update MaximumBoundedArray.java

--- MaximumBoundedArray.java
+++ MaximumBoundedArray.java
@@ -1,16 +1,19 @@
 //https://aonecode.com/interview-question/maximum-bounded-array
 
+//Also known as winning sequence
+
 public static int[] maximumBoundedArray(int n, int lowBound, int upBound){
         int result[] = new int[n];
         int diff = upBound-lowBound;
-        if(diff < n/2)
-            return result;
-
+        if(diff < n/2){
+            return new int[]{-1};
+        }
+        
         int mid = 0;
-        if(n%2 == 0)
-            mid = n/2 -1;
+        if(n%2 == 1 && diff == n/2)
+            mid = n / 2;
         else
-            mid = n/2;
+            mid = n/2-1;
 
         result[mid] = upBound;
         for(int i = mid-1;i>=0;i--){
